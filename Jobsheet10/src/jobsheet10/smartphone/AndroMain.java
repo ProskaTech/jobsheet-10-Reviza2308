3eb92b63d4d03e19ddb1251bfec6d223e57614e2
Reviza2308 <112670494+Reviza2308@users.noreply.github.com>
2022-11-03 20:41:57 +0700

    Add files via upload

---
+++ Jobsheet10/src/jobsheet10/smartphone/AndroMain.java
@@ -0,0 +1,17 @@
+package jobsheet10.smartphone;
+
+/**
+ *
+ * Created by 21343031_Muhammad Reviza Dekry
+ */
+
+public class AndroMain {
+    public static void main (String[] args){
+        Android realme = new Android();
+        
+        realme.nyala();
+        realme.panggilan();
+        realme.shutdown();
+        realme.sms();
+    }
+}
